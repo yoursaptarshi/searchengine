d5624e600adf5a0da2142e20381779a53cb2cdfc
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-01-21 16:49:35 +0530

    Update memberships.js

--- frontend/src/Actions/memberships.js
+++ frontend/src/Actions/memberships.js
@@ -35,48 +35,46 @@ export const allMemberships =()=>async(dispatch)=>{
     }
 }
 
-export const buyMembership = (membership)=>async (dispatch)=>{
+export const buyMembership = (membership) => async (dispatch) => {
     try {
         dispatch({
-            type:"buyMembershipRequest"
-        })
-        
+            type: "buyMembershipRequest",
+        });
+
         const stripe = await loadStripe('pk_test_51K7jzHSEu1sDfKjEHqOo5sOaKQEqcwEiaYWwnc1oofaq5q0V45uWPvIS2DYLRf7YqeorI57Man6bxwUpUyyL7Sgh00bUwajwHw');
-        
-        const response = await axios.post("/api/v1/buy-membership",{membership_name:membership});
-        
-        
-        if(response){
-            const {sessionId} = response.data;
-            console.log(sessionId)
+
+        const response = await axios.post("/api/v1/buy-membership", { membership_name: membership });
+
+        if (response.status === 200) {
+            const { sessionId } = response.data;
+
             const stripeSession = await stripe.redirectToCheckout({
-                sessionId:sessionId,
-            })
-            
+                sessionId: sessionId,
+            });
+
             if (stripeSession.error) {
                 console.error('Error redirecting to checkout:', stripeSession.error);
-              }
-              else{
-                await axios.post("/api/v1/update-membership",{membership_name:membership});
-                
                 dispatch({
-                    type:"buyMembershipSuccess"
-                })
-              }
-        }
-        else {
-            console.error('Membership purchase failed:', response.message);
-            dispatch({
-                type:"buyMembershipFailure",
+                    type: "buyMembershipFailure",
+                });
+            } else {
+                await axios.post("/api/v1/update-membership", { membership_name: membership });
                 
-            })
-          }
+                console.log(2);
+                dispatch({
+                    type: "buyMembershipSuccess",
+                });
+            }
+        } else {
+            console.error('Membership purchase failed:', response.data.message);
+            dispatch({
+                type: "buyMembershipFailure",
+            });
+        }
     } catch (error) {
-        
+        console.error('Error in buyMembership:', error.message);
         dispatch({
-            type:"buyMembershipFailure",
-            
-        })
-        
+            type: "buyMembershipFailure",
+        });
     }
-}
+};
