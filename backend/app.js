6183322ff1b8fc283b816de4616c07b12cf0aeca
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-01-22 10:49:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const user = require("./routes/user");
 const crawled = require("./routes/crawled");
-const memberships = require("./routes/memberships")
+const memberships = require("./routes/memberships");
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-
 const path = require("path");
+
 app.use(cors());
 
-
-
-//using middlewares
-
+// Using middlewares
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-
+// Move the raw body capture middleware after body-parsing middleware
 app.use((req, res, next) => {
     req.rawBody = '';
     req.setEncoding('utf8');
 
-    req.on('data', (chunk) => { 
+    req.on('data', (chunk) => {
         req.rawBody += chunk;
     });
 
@@ -31,17 +28,15 @@ app.use((req, res, next) => {
     });
 });
 
-//using routes
-app.use("/api/v1",user);
-app.use("/api/v1",crawled)
-app.use("/api/v1",memberships)
-
+// Using routes
+app.use("/api/v1", user);
+app.use("/api/v1", crawled);
+app.use("/api/v1", memberships);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
-
-module.exports=app;
+module.exports = app;
