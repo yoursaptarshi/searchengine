8e54a5d43b6cb986f0b6b03e5a3f9570e6529775
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-01-22 12:00:29 +0530

    Update memberships.js

--- backend/controllers/memberships.js
+++ backend/controllers/memberships.js
@@ -130,7 +130,7 @@ exports.updateMembership = async (req, res) => {
         const payload = req.body;
         try {
             
-            event = stripe.webhooks.constructEvent(JSON.stringify(payload), sig, 'whsec_dxUOg47TuAKwmmnjEbu5fe2s22Go6Nl8');
+            event = stripe.webhooks.constructEvent(req.rawBody, sig, 'whsec_dxUOg47TuAKwmmnjEbu5fe2s22Go6Nl8');
             //whsec_dxUOg47TuAKwmmnjEbu5fe2s22Go6Nl8
             //whsec_d9b0cc47c810a6254a6bfb0c65731fdf82f5c43e46ceb24ddc949f575dd78e5a
         } catch (err) {
